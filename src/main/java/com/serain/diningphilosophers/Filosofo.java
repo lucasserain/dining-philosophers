073a4f5ecdaf8dc1e002ae19a8b7dff0feef5e8a
Lucas Serain <lucas.serain@outlook.com>
2020-09-15 21:58:33 -0300

    Time para tempo de comer e pensar.

--- src/main/java/com/serain/diningphilosophers/Filosofo.java
+++ src/main/java/com/serain/diningphilosophers/Filosofo.java
@@ -4,30 +4,33 @@ public class Filosofo implements Runnable {
 
 	private Object garfoEsquerdo;
 	private Object garfoDireito;
+	private Integer ciclo = 0;
 
 	public Filosofo(Object garfoEsquerdo, Object garfoDireito ){
 		this.garfoEsquerdo = garfoEsquerdo;
 		this.garfoDireito = garfoDireito;
 	}
 
-	private void agir(String acao) throws InterruptedException {
-		System.out.println(
-				Thread.currentThread().getName() + " " + acao);
-		Thread.sleep(((int) (Math.random() * 100)));
+	private void agir(String acao,Integer status) throws InterruptedException {
+		System.out.println(Thread.currentThread().getName() + " " + acao);
+		if(status == 1){
+			Thread.sleep((long) 1);
+		}
 	}
 	
 	@Override
 	public void run() {
 		try{
 			while(true){
-				agir(System.nanoTime() + ": Pensando");
+				System.out.println("Ciclo:" + ++ciclo);
+				agir(System.nanoTime() + ": Pensando",1);
 				synchronized (garfoEsquerdo){
-					agir(System.nanoTime() + ": Pegou o garfo esquerdo");
+					agir(System.nanoTime() + ": Pegou o garfo esquerdo",2);
 					synchronized (garfoDireito){
-						agir(System.nanoTime() + ": Pegou o garfo direito - Comendo...");
-						agir(System.nanoTime() + ": Largou o garfo direito");
+						agir(System.nanoTime() + ": Pegou o garfo direito - Comendo...",3);
+						agir(System.nanoTime() + ": Largou o garfo direito",4);
 					}
-					agir(System.nanoTime() + ": Largou o garfo esquerdo. Voltando a pensar...");
+					agir(System.nanoTime() + ": Largou o garfo esquerdo. Voltando a pensar...",5);
 				}
 			}
 		} catch (InterruptedException e) {
