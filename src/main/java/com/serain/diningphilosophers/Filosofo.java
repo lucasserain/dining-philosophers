aaf015cb42f838f8d35d0c2abb94deb227cf1c1f
Lucas Serain <lucas.serain@outlook.com>
2020-09-20 22:54:56 -0300

    Adição de garfo ao meio.

--- src/main/java/com/serain/diningphilosophers/Filosofo.java
+++ src/main/java/com/serain/diningphilosophers/Filosofo.java
@@ -4,17 +4,19 @@ public class Filosofo implements Runnable {
 
 	private Object garfoEsquerdo;
 	private Object garfoDireito;
+	private Object garfoMeio;
 	private Integer ciclo = 0;
 
-	public Filosofo(Object garfoEsquerdo, Object garfoDireito ){
+	public Filosofo(Object garfoEsquerdo, Object garfoDireito, Object garfoMeio ){
 		this.garfoEsquerdo = garfoEsquerdo;
 		this.garfoDireito = garfoDireito;
+		this.garfoMeio = garfoMeio;
 	}
 
 	private void agir(String acao,Integer status) throws InterruptedException {
 		System.out.println(Thread.currentThread().getName() + " " + acao);
 		if(status == 1){
-			Thread.sleep((long) 1);
+			Thread.sleep((long) 0.9);
 		}else if(status == 4){
 			Thread.sleep((long) 0.1);
 		}
@@ -32,6 +34,9 @@ public class Filosofo implements Runnable {
 						agir(System.nanoTime() + ": Pegou o garfo direito - Comendo...",3);
 						agir(System.nanoTime() + ": Largou o garfo direito",4);
 					}
+					synchronized (garfoMeio){
+                        agir(System.nanoTime() + ": Pegou o garfo do meio",6);
+                    }
 					agir(System.nanoTime() + ": Largou o garfo esquerdo. Voltando a pensar...",5);
 				}
 			}
@@ -43,7 +48,7 @@ public class Filosofo implements Runnable {
 	public static void main(String[] args) throws Exception {
 
 		Filosofo[] filosofos = new Filosofo[5];
-		Object[] garfos = new Object[filosofos.length];
+		Object[] garfos = new Object[6];
 
 		for (int i = 0; i < garfos.length; i++) {
 			garfos[i] = new Object();
@@ -51,9 +56,10 @@ public class Filosofo implements Runnable {
 
 		for (int i = 0; i < filosofos.length; i++) {
 			Object garfoEsquerdo = garfos[i];
+            Object garfoMeio = garfos[i];
 			Object garfoDireito = garfos[(i + 1) % garfos.length];
 
-			filosofos[i] = new Filosofo(garfoEsquerdo, garfoDireito);
+			filosofos[i] = new Filosofo(garfoEsquerdo, garfoDireito,garfoMeio);
 
 			Thread t
 					= new Thread(filosofos[i], "Filósofo " + (i + 1));
